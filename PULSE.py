e9140f40d55c808665798c93a922c67b1646c707
Tomasz Zieliński <tomasz.zielinski@gmail.com>
2020-06-22 11:55:21 +0200

    url change

--- PULSE.py
+++ PULSE.py
@@ -20,7 +20,7 @@ class PULSE(torch.nn.Module):
         cache_dir = Path(cache_dir)
         cache_dir.mkdir(parents=True, exist_ok = True)
         if self.verbose: print("Loading Synthesis Network")
-        with open_url("https://drive.google.com/uc?id=1TCViX1YpQyRsklTVYEJwdbmK91vklCo8", cache_dir=cache_dir, verbose=verbose) as f:
+        with open_url("https://drive.google.com/uc?id=1oJRF5oYaOBrAMx9V6WoezhcinaAOo0v3", cache_dir=cache_dir, verbose=verbose) as f:
             self.synthesis.load_state_dict(torch.load(f))
 
         for param in self.synthesis.parameters():
